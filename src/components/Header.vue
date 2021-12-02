6a4f971f9e6cb352361597b677b449c39051a673
Loïc Monard <lmonard@simco.fr>
2021-12-02 23:12:45 +0100

    feature(map): add initial map to home
    Signed-off-by: Loïc Monard <lmonard@simco.fr>

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <img id="logo" src="../assets/crabe.png" />
+    <span id="title">
+      Crabe cool
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 64px;
+  color: #fff;
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 24px;
+  #logo {
+    height: 32px;
+  }
+  #title {
+    font-family: "Pulp Display";
+    text-transform: uppercase;
+    font-size: 16px;
+  }
+}
+</style>
